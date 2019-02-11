257957b713a03c716a18ae0f0596635c78222809
sunce <king@sunce.xin>
2019-02-11 21:23:04 +0800

    Initial commit

---
+++ src/main/java/xin/sunce/mybatis/Application.java
@@ -0,0 +1,28 @@
+package xin.sunce.mybatis;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ce.sun
+ * @descreption main方法启动配置生成
+ */
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<>();
+        boolean overwrite = true;
+        File configFile = new File("src/main/resources/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
---
+++ src/test/java/xin/sunce/mybatis/ApplicationTest.java
@@ -0,0 +1,74 @@
+package xin.sunce.mybatis;
+
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+import xin.sunce.mybatis.dao.CustomerDao;
+import xin.sunce.mybatis.entity.Customer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Logger;
+
+public class ApplicationTest {
+
+    private SqlSessionFactory sessionFactory;
+
+    private Logger LOGGER = Logger.getLogger(ApplicationTest.class.getName());
+
+    @Before
+    public void getSqlSessionFactory() throws IOException {
+        String configPath = "mybatis.xml";
+        InputStream resourceAsStream = Resources.getResourceAsStream(configPath);
+        sessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+    }
+
+    @Test
+    public void testInsert() {
+        SqlSession sqlSession = sessionFactory.openSession();
+        try {
+            CustomerDao mapper = sqlSession.getMapper(CustomerDao.class);
+            Customer customer = new Customer();
+            customer.setId(1L);
+            customer.setOptimistic(0L);
+            customer.setName("Customer.ce");
+            LOGGER.info("insert into table Customer: " + customer.toString());
+            mapper.insert(customer);
+            sqlSession.commit();
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testQuery() {
+        SqlSession sqlSession = sessionFactory.openSession();
+        try {
+            CustomerDao mapper = sqlSession.getMapper(CustomerDao.class);
+            Customer customer = mapper.selectByPrimaryKey(1L);
+            LOGGER.info("query table Customer result: " + customer.toString());
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testUpdate() {
+        SqlSession sqlSession = sessionFactory.openSession();
+        try {
+            CustomerDao mapper = sqlSession.getMapper(CustomerDao.class);
+            Customer customer = new Customer();
+            customer.setId(1L);
+            customer.setName("ce.Customer");
+            int num = mapper.updateByPrimaryKey(customer);
+            LOGGER.info("update table Customer result: " + customer.toString() + ",effected " + num + " rows");
+            sqlSession.commit();
+        } finally {
+            sqlSession.close();
+        }
+    }
+}
