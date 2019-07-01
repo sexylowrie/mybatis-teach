9e4028d3e45a63b323790a091d2406fb3804c398
sunce <king@sunce.xin>
2019-07-01 18:05:06 +0800

    更新测试用例

--- springboot-mybatis/src/test/java/xin/sunce/mybatis/dao/CustomerDaoTest.java
+++ springboot-mybatis/src/test/java/xin/sunce/mybatis/dao/CustomerDaoTest.java
@@ -5,11 +5,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import xin.sunce.mybatis.Application;
 import xin.sunce.mybatis.entity.Customer;
+
+import javax.annotation.Resource;
 
 
 @RunWith(SpringRunner.class)
@@ -18,7 +19,7 @@ public class CustomerDaoTest {
 
     private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-    @Autowired
+    @Resource
     private CustomerDao customerDao;
 
     @Test
